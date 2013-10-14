d8661905ff9b18b614424e570a8e8fb60c90a9c8
Max Cai <maxtroy@google.com>
2013-10-14 16:28:08 +0100

    Make generated code more aligned with Google Java style.
    - Blank line after opening a message class (but not an enum interface).
    - Let all code blocks insert blank lines before themselves. This applies to
      'package' statement, all message classes, enum classes or constant groups,
      extensions, bitfields, proto fields (one block per field; i.e. accessors
      don't have blank lines among them), and basic MessageNano methods. In this
      case we don't need to guess what the next block is and create blank lines
      for it.
    - Fixed some newline/indent errors.
    - Only one SuppressWarnings("hiding") per file.
    
    Change-Id: I865f52ad4fb6ea3b3a98b97ac9d78d19fc46c858

--- src/google/protobuf/compiler/javanano/javanano_enum_field.cc
+++ src/google/protobuf/compiler/javanano/javanano_enum_field.cc
@@ -111,11 +111,11 @@ GenerateClearCode(io::Printer* printer) const {
 void EnumFieldGenerator::
 GenerateMergingCode(io::Printer* printer) const {
   printer->Print(variables_,
-    "  this.$name$ = input.readInt32();\n");
+    "this.$name$ = input.readInt32();\n");
 
   if (params_.generate_has()) {
     printer->Print(variables_,
-      "  has$capitalized_name$ = true;\n");
+      "has$capitalized_name$ = true;\n");
   }
 }
 
@@ -276,7 +276,8 @@ GenerateMergingCode(io::Printer* printer) const {
       "input.popLimit(limit);\n");
   } else {
     printer->Print(variables_,
-      "int arrayLength = com.google.protobuf.nano.WireFormatNano.getRepeatedFieldArrayLength(input, $tag$);\n"
+      "int arrayLength = com.google.protobuf.nano.WireFormatNano\n"
+      "    .getRepeatedFieldArrayLength(input, $tag$);\n"
       "int i = this.$name$.length;\n"
       "int[] newArray = new int[i + arrayLength];\n"
       "System.arraycopy(this.$name$, 0, newArray, 0, i);\n"
